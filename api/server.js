0a01bb00bd27e0e910844cd000797a8297427870
Spencer Wong <sawong887@gmail.com>
2023-12-12 14:09:41 -0800

    newer version

--- api/server.js
+++ api/server.js
@@ -1,3 +1,113 @@
-// BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id);
+        if (!possibleUser) {
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist',
+            });
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: 'Please provide name and bio for the user'
+                });
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body);
+                res.status(200).json(updatedUser);
+            }
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'error updating user',
+            error: error.message,
+            stack: error.stack,
+        });
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        });
+    } else {
+        const deletedUser = await User.remove(possibleUser.id);
+        res.status(200).json(deletedUser);
+}
+    } catch (error) {
+        res.status(500).json({
+            message: 'error deleting user',
+            error: error.message,
+            stack: error.stack,
+        });
+    }
+});
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        });
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser);
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'error creating user',
+            error: error.message,
+            stack: error.stack,
+        });
+    });
+    }
+});
+
+server.get('/api/users', (req, res) => {
+    User.find()
+    .then(users => {
+        res.json(users);
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'error getting users',
+            error: error.message,
+            stack: error.stack,
+        });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: 'The user with the specified ID does not exist',
+                });
+            }
+            res.json(user);
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error getting user',
+                error: error.message,
+                stack: error.stack,
+            });
+        });
+});
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+});
+
+module.exports = server; 
